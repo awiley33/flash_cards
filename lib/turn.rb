edd10808e24f3d1726a0bfca4dbf4187f0937506
Anna Wiley <awiley33@gmail.com>
2023-05-08 22:49:48 -0500

    Define turn class

---
+++ lib/turn.rb
@@ -0,0 +1,24 @@
+class Turn
+    attr_reader :guess, :card
+    def initialize(guess, card)
+        @guess = guess
+        @card = card
+    end
+
+    def correct?
+        if @guess = @card.answer
+            p true
+        else
+            p false
+        end
+    end
+    
+    def feedback
+        if @guess = @card.answer
+            p "Correct!"
+        else
+            p "Incorrect."
+        end
+    end
+
+end
